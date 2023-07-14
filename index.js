8e115d5f66daba2654725e79290f05a4bc02c19d
tomutanyi <tomutanyi@gmail.com>
2023-07-14 11:49:09 +0300

    Commited assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+
+function superbowlWin(record){
+    for (const i of record){
+        if (i.result == 'W'){
+            return i.year
+        }
+    }
+}
